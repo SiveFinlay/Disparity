78caddec76e5522dd66cca284ac11eaf67d81252
Sive Finlay <sfinlay@tcd.ie>
2014-07-23 12:45:14 +0100

    Finished Dean Adams' back  transformation method for simulating expected disparity. I need to fix issues with different versions of vcv.phylo function in the updated versions of geiger before I can up-scale the simulations on the alien

--- analyses/disparity_simulation_backtransform.r
+++ analyses/disparity_simulation_backtransform.r
@@ -10,10 +10,16 @@
     # simulating error in landmark locations for each landmark. So what you could do is take the vector of simulated values for each species,
     # add the overall reference specimen (overall mean from GPA) to these. Now you have simulated BM deviations from the overall mean form.
     # Then do GPA of the simulated dataset and calculate disparity. I believe this would put things back into Procrustes-comparable units.
-    
 
+#This method seems to work but I need to up-scale it to more simulations
+  #However first I need to sort out the problem of dealing with different versions of geiger
+      
+#-------------------------------------------------------
 library(ape)
-library(geiger)
+library(geiger)   
+#NB: The new version of geiger (geiger_2.0.3.)creates different outputs for the vcv.phylo function
+      #it produces vcv matrices with species as the rows and columns, not traits
+      #I have an older geiger version (geiger_1.99-3.1) installed on my laptop but the new version is the alien
 library(geomorph)
 
 
@@ -21,14 +27,26 @@ source("C:/Users/sfinlay/Desktop/Thesis/Disparity/functions/DisparityFunctions_V
 source("C:/Users/sfinlay/Desktop/Thesis/Disparity/functions/PValueFunction_FromDistribution.r")
 source("C:/Users/sfinlay/Desktop/Thesis/Disparity/functions/Disparity_general_functions.r" )
 
+#On cyberman (alien as remote server)
+  #source("~/Disparity/functions/Disparity_general_functions.r")
+  #source("~/Disparity/functions/DisparityFunctions_Variance_Range.r")
+  #source("~/Disparity/functions/PvalueFunction_FromDistribution.r")
+
+
+
+
+
 #SkDors
 #1) Phylogenies
    setwd("C:/Users/sfinlay/Desktop/Thesis/Disparity/output/phylogenies")
+   #setwd("~/Disparity/output/phylogenies")
    mytrees <- read.tree("SkDors_tenrec+gmole_101trees.phy")
 
 #2) Data
    setwd("C:/Users/sfinlay/Desktop/Thesis/Disparity/output/shape_data/skdors")
-
+   #setwd("~/Disparity/output/shape_data/skdors")
+   
+   
   #2a) All tenrecs and golden moles
       #shape coordinates
       sps.mean <- dget(file="SkDors_tenrec+gmole_sps.mean.txt")
@@ -50,11 +68,12 @@ source("C:/Users/sfinlay/Desktop/Thesis/Disparity/functions/Disparity_general_fu
 #-------------------------------------------------
 #Calculate observed disparity
 
-#PCA of mean shape values for each species
+#PCA of mean shape values for each species     (NB: L.Harmon said to do a phylogenetic PCA)
   obsPC <- prcomp(twoDshape)
 
 #select the PC axes which correspond to 95% of the variation
   obsPC95 <- selectPCaxes.prcomp(obsPC, 0.956)
+
 
 #Observed disparity for tenrecs
   tenrecPC <- obsPC95[which(tax$Family=="Tenrecidae"),]
@@ -71,12 +90,6 @@ source("C:/Users/sfinlay/Desktop/Thesis/Disparity/functions/Disparity_general_fu
   gmole.prodvar <- PCprodvar(gmolePC)
   gmole.sumrange <- PCsumrange(gmolePC)
   gmole.prodrange <- PCprodrange(gmolePC)
-
-#Ratios of observed tenrec/gmole disparity
-  obs.sumvar.ratio <- tenrec.sumvar/gmole.sumvar
-  obs.prodvar.ratio <- tenrec.prodvar/gmole.prodvar
-  obs.sumrange.ratio <- tenrec.sumrange/gmole.sumrange
-  obs.prodrange.ratio <- tenrec.prodrange/gmole.prodrange
 
 #---------------------------------------------------------------
 #Simulate data from VCV matrices
@@ -100,7 +113,7 @@ source("C:/Users/sfinlay/Desktop/Thesis/Disparity/functions/Disparity_general_fu
 #Simulate shape evolution on each phylogeny
   shape.sim <- as.list(rep(NA,length(mytrees)))
     for (i in 1: length(mytrees)){
-      shape.sim[[i]] <- sim.char(mytrees[[i]], varcov[[i]],nsim=5,model="BM")         #scale up the simulations later
+      shape.sim[[i]] <- sim.char(mytrees[[i]], varcov[[i]],nsim=50,model="BM")         #scale up the simulations later
     }
 
 #Combine simulations into one list
@@ -150,7 +163,8 @@ simlist <- list.arrays.to.matrices(shape.sim)
       sim.GPA[[i]] <- gpagen(sim.add.mshape[[i]], ProcD=TRUE)
     }
 
-#Calculate disparity for each of these sets of Procrustes-superimposed simulated data
+#PCA for each of these sets of Procrustes-superimposed simulated data  
+  #(NB: This takes ages to run because it creates a PCA graph for each one; maybe it would be better to use prcomp?)
   sim.PCA <- NULL
     for (i in 1:length(sim.GPA)){
       sim.PCA[[i]] <- plotTangentSpace(sim.GPA[[i]]$coords)
@@ -161,4 +175,53 @@ simlist <- list.arrays.to.matrices(shape.sim)
     for (i in 1:length(sim.PCA)){
       sim.PC95axes[[i]] <- selectPCaxes(sim.PCA[[i]], 0.956, tax.sorted$Binomial)
     }
-    #BREAKS AT NUMBER 167: FIRST PC axis is above the threshold so I need to fix that function
+
+#Calculate disparity for each simulated data set
+  #Tenrecs
+  tenrec.rows <- which(tax.sorted$Family == "Tenrecidae") 
+  
+  #tenrec rows of the PC matrices
+    tenrec.sim.PC <- NULL
+      for (i in 1:length(sim.PC95axes)){
+        tenrec.sim.PC[[i]] <- sim.PC95axes[[i]][tenrec.rows,]
+      }
+   
+  #Calculate tenrec disparity   
+         
+  sim.tenrec.sumvar <- unlist(calc.each.list(mylist=tenrec.sim.PC, calculation=PCsumvar))
+  sim.tenrec.prodvar <- unlist(calc.each.list(mylist=tenrec.sim.PC, calculation=PCprodvar))
+   
+  sim.tenrec.sumrange <- unlist(calc.each.list(mylist=tenrec.sim.PC, calculation=PCsumrange))
+  sim.tenrec.prodrange <- unlist(calc.each.list(mylist=tenrec.sim.PC, calculation=PCprodrange))
+
+
+  #Golden moles
+    gmole.rows <- which(tax.sorted$Family == "Chrysochloridae") 
+  
+  #golden mole rows of the PC matrices
+    gmole.sim.PC <- NULL
+      for (i in 1:length(sim.PC95axes)){
+        gmole.sim.PC[[i]] <- sim.PC95axes[[i]][gmole.rows,]
+      }
+   
+  #Calculate gmoledisparity   
+         
+  sim.gmole.sumvar <- unlist(calc.each.list(mylist=gmole.sim.PC, calculation=PCsumvar))
+  sim.gmole.prodvar <- unlist(calc.each.list(mylist=gmole.sim.PC, calculation=PCprodvar))
+   
+  sim.gmole.sumrange <- unlist(calc.each.list(mylist=gmole.sim.PC, calculation=PCsumrange))
+  sim.gmole.prodrange <- unlist(calc.each.list(mylist=gmole.sim.PC, calculation=PCprodrange))
+
+#----------------------------------------------
+#Compare observed and simulated disparity
+  #Tenrecs   
+    tenrec.sumvar.p <- pvalue.dist(distribution=sim.tenrec.sumvar, obs.val=tenrec.sumvar)
+    tenrec.prodvar.p <- pvalue.dist(distribution=sim.tenrec.prodvar, obs.val=tenrec.prodvar) 
+    tenrec.sumrange.p <- pvalue.dist(distribution=sim.tenrec.sumrange, obs.val=tenrec.sumrange) 
+    tenrec.prodrange.p <- pvalue.dist(distribution=sim.tenrec.prodrange, obs.val=tenrec.prodrange) 
+    
+  #Golden moles   
+    gmole.sumvar.p <- pvalue.dist(distribution=sim.gmole.sumvar, obs.val=gmole.sumvar)
+    gmole.prodvar.p <- pvalue.dist(distribution=sim.gmole.prodvar, obs.val=gmole.prodvar) 
+    gmole.sumrange.p <- pvalue.dist(distribution=sim.gmole.sumrange, obs.val=gmole.sumrange) 
+    gmole.prodrange.p <- pvalue.dist(distribution=sim.gmole.prodrange, obs.val=gmole.prodrange)   
